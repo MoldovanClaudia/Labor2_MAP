7aa7b52feb2c25ae8327469d362af4654e238b67
Alexandra Moldovan <e11834541@student.tuwien.ac.at>
2023-12-18 19:35:23 +0100

    .

--- src/Domain/Observable.java
+++
@@ -1,9 +0,0 @@
-package Domain;
-
-public interface Observable {
-    void notifyObservers(String propertyName, Object value);
-
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-
-}
--- src/Domain/Observer.java
+++
@@ -1,5 +0,0 @@
-package Domain;
-
-public interface Observer {
-    void update(Observable observable, String propertyName, Object value);
-}
